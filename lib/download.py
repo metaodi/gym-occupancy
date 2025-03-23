e5c4b006e1e7e698f9d6650c983248fcd30b35b4
Stefan Oderbolz <oderbolz@gmail.com>
2025-03-23 10:34:52 +0100

    Add initial files

---
+++ lib/download.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import logging
+import requests
+import time
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+
+log = logging.getLogger(__name__)
+
+
+def _download_request(url, verify=True):
+    retry_strategy = Retry(
+        total=5,
+        backoff_factor=2,
+        status_forcelist=[403, 429, 500, 502, 503, 504],
+        allowed_methods=["HEAD", "GET", "OPTIONS"]
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    http = requests.Session()
+    http.mount("https://", adapter)
+    http.mount("http://", adapter)
+    headers = {'user-agent': 'Mozilla Firefox Mozilla/5.0; metaodi website-monitor at github'}
+    r = http.get(url, headers=headers, timeout=20, verify=verify)
+    r.raise_for_status()
+    return r
+
+
+def download(url, encoding='utf-8', verify=True):
+    r = _download_request(url, verify=verify)
+    if encoding:
+        r.encoding = encoding
+    return r.text
+
+
+def download_content(url, verify=True):
+    r = _download_request(url, verify=verify)
+    return r.content
+
+
+def jsondownload(url, verify=True):
+    r = _download_request(url, verify=verify)
+    return r.json()
+
+
+def download_file(url, path, verify=True):
+    r = _download_request(url, verify=verify)
+    with open(path, 'wb') as f:
+        for chunk in r.iter_content(1024):
+            f.write(chunk)
+
+
+def download_with_selenium(url, selector):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+
+    driver.get(url)
+    try:
+        wait = WebDriverWait(driver, 20)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
+        time.sleep(5)
+        content = driver.page_source
+    except TimeoutException:
+        # if the selector was not found return a static string
+        log.info(f"selector '{selector}' not found!")
+        content = f"selector '{selector}' not found"
+    finally:
+        driver.quit()
+
+    return content
---
+++ scrape.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""Hash of a website selector
+
+Usage:
+  scrape.py --file <path-to-csv> [--verbose]
+  scrape.py (-h | --help)
+  scrape.py --version
+
+Options:
+  -h, --help                    Show this screen.
+  --version                     Show version.
+  -f, --file <path-to-csv>    Path to CSV file.
+  --verbose                     Option to enable more verbose output.
+"""
+
+import logging
+import sys
+from bs4 import BeautifulSoup
+from docopt import docopt
+from lib import download as dl
+from datetime import datetime
+
+
+def get_occupancy(url):
+    selector = "[data-visitors]"
+    content = dl.download_with_selenium(url, selector)
+    soup = BeautifulSoup(content, "html.parser")
+    span = soup.select_one(selector)
+    if not span:
+        log.error(f"Selector {selector} not found in {url}")
+        sys.exit(1)
+    log.debug(f"span with '{span.text}'")
+    try:
+        occupancy = int(span.text)
+    except ValueError:
+        occupancy = None
+
+    return occupancy
+
+
+def read_from_csv(csv_path):
+    rows = []
+    with open(csv_path) as f:
+        reader = DictReader(f)
+        rows = [row for row in reader]
+    return rows
+
+
+def write_to_csv(csv_path, header, rows):
+    with open(csv_path, 'w', encoding="utf-8") as f:
+        writer = csv.DictWriter(
+            f,
+            header,
+            delimiter=',',
+            quotechar='"',
+            lineterminator='\n',
+            quoting=csv.QUOTE_NONNUMERIC
+        )
+        log.info("Start writing CSV")
+        writer.writeheader()
+        writer.writerows(rows)
+
+
+log = logging.getLogger(__name__)
+arguments = docopt(__doc__, version="Get text from website 1.0")
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.captureWarnings(True)
+
+if arguments["--verbose"]:
+    log.setLevel(logging.DEBUG)
+
+file_path = arguments["--file"]
+rows = read_from_csv(file_path)
+updated_rows = []
+for row in rows:
+    try:
+        row["timestamp_utc"] = datetime.utcnow().isoformat()
+        row["occupancy"] = get_occupancy(row["url"])
+    finally:
+        updated_rows.append(row)
+
+header = [
+    'timestamp_utc',
+    'url',
+    'training_area_m2',
+    'gym',
+    'occupancy',
+]
+write_to_csv(file_path, header, updated_rows)
