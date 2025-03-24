f0caced2abb06ab7b9f5603bd0003b3a822d1305
Stefan Oderbolz <oderbolz@gmail.com>
2025-03-24 19:31:46 +0100

    Update extract_git_history.py

--- extract_git_history.py
+++ extract_git_history.py
@@ -3,7 +3,7 @@
 """Extract file history from git
 
 Usage:
-  extract_git_history.py --file <file-path> --output <output-path>
+  extract_git_history.py --file <file-path> --output <output-path> [--output-format <format>]
   extract_git_history.py (-h | --help)
   extract_git_history.py --version
 
@@ -13,12 +13,13 @@ Options:
   --version                    Show version.
   -f, --file <file-path>       Path to the git controlled file.
   -o, --output <output-path>   Path to the output file.
+  -f, --output-format <format> Format of the output file [default: csv].
 """
 
 from docopt import docopt
 import git
 import pandas as pd
-arguments = docopt(__doc__, version='convert_sqlite_to_pkl.py 1.0')
+arguments = docopt(__doc__, version='extract_git_history.py 1.0')
 
 input_file = arguments["--file"]
 output_file = arguments["--output"]
